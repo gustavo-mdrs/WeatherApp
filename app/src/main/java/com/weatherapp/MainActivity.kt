28d84c280cbf261a0c388404dfd7cbeaad0e6cb6
Gustavo Medeiros <cipguga@gmail.com>
2025-12-13 10:42:58 -0300

    Pratica 2 - parte 1

--- app/src/main/java/com/weatherapp/MainActivity.kt
+++ app/src/main/java/com/weatherapp/MainActivity.kt
@@ -1,30 +1,14 @@
 package com.weatherapp
 
-import android.app.Activity
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.LocalActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import com.weatherapp.ui.theme.HomePage
 import com.weatherapp.ui.theme.WeatherAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,31 +20,6 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     HomePage(modifier = Modifier.padding(innerPadding))
                 }
-            }
-        }
-    }
-}
-@Preview(showBackground = true)
-@Composable
-fun HomePage(modifier: Modifier = Modifier) {
-    var email by rememberSaveable { mutableStateOf("") }
-    var password by rememberSaveable { mutableStateOf("") }
-    val activity = LocalActivity.current as Activity
-    Column(
-        modifier = modifier.padding(16.dp).fillMaxSize()
-    ) {
-        Text(
-            text = "Boas Vindas Usuário!",
-            fontSize = 24.sp
-        )
-
-        Spacer(modifier = modifier.size(44.dp))
-
-        Row(modifier = modifier) {
-            Button( onClick = {
-                activity.finish()
-            } ) {
-                Text("Sair")
             }
         }
     }
